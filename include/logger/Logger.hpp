a3b10873c22f621844cea84d3de8cdf8dc9e3c27
Vuk <104253978+Amizic@users.noreply.github.com>
2026-05-18 02:40:10 +0200

    Add files via upload

--- include/logger/Logger.hpp
+++ include/logger/Logger.hpp
@@ -9,43 +9,43 @@
 
 #ifdef _WIN32
 
-    #ifdef LOGGER_EXPORTS
-        #define LOGGER_API __declspec(dllexport)
-    #elif defined(LOGGER_STATIC_DEFINE)
-        #define LOGGER_API
-    #else
-        #define LOGGER_API __declspec(dllimport)
-    #endif
-    
-    #include <windows.h>
-    
+#ifdef LOGGER_EXPORTS
+#define LOGGER_API __declspec(dllexport)
+#elif defined(LOGGER_STATIC_DEFINE)
+#define LOGGER_API
 #else
-    #define LOGGER_API
-    // Linux/macOS console colors using ANSI escape codes
-    #define FOREGROUND_RED          0x0001
-    #define FOREGROUND_GREEN        0x0002  
-    #define FOREGROUND_BLUE         0x0004
-    #define FOREGROUND_INTENSITY    0x0008
-    typedef unsigned short WORD;
+#define LOGGER_API __declspec(dllimport)
 #endif
 
-class LOGGER_API Logger {
+#include <windows.h>
+
+#else
+#define LOGGER_API
+// Linux/macOS console colors using ANSI escape codes
+#define FOREGROUND_RED          0x0001
+#define FOREGROUND_GREEN        0x0002
+#define FOREGROUND_BLUE         0x0004
+#define FOREGROUND_INTENSITY    0x0008
+typedef unsigned short WORD;
+#endif
+
+class LOGGER_API Logger {   // <-- Added LOGGER_API here (fix 1)
 public:
     // Constructor & Destructor
     Logger(const std::string& loggerName, std::mutex& consoleMutex);
     Logger(const std::string& loggerName);
     ~Logger();
-    
+
     // File logging methods
     void enableFileLogging(const std::string& filePath);
     void disableFileLogging();
-    
+
     // Simple logging methods
     void logMessage(const std::string& message);
     void logSuccess(const std::string& message);
     void logWarning(const std::string& message);
     void logError(const std::string& message);
-    
+
 private:
     // Helper methods
     void logToConsole(const std::string& prefix, const std::string& message, WORD color);
@@ -54,23 +54,22 @@ private:
     void resetConsoleColor();
     std::string getCurrentTimestamp();
     std::string formatLogLine(const std::string& prefix, const std::string& message);
-    
+
     // Platform-specific console initialization
     void initConsole();
-    
+
     // Member variables
     std::string loggerName;
     std::mutex internalMutex;
     std::mutex& consoleMutex;
-    
-#ifdef _WIN32
+
+    #ifdef _WIN32
     HANDLE consoleHandle;
-#else
+    #else
     bool useColors;
-#endif
-    
+    #endif
+
     // File logging
     std::ofstream logFile;
     std::mutex fileMutex;
-
 };
